6a8eb7ef45ad0d7d4e698144f39844fba3c53d02
Aron Ambrosiani <aron@ambrosiani.se>
2017-11-28 00:01:03 +0100

    adding example files

---
+++ user-config.example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# This is a sample file. You should use generate_user_files.py
+# to create your user-config.py file.
+
+mylang = 'commons'
+family = 'commons'
+usernames['commons']['commons'] = 'Example'
+password_file = "user-password.py"
